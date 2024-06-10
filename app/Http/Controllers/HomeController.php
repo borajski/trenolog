71de45766abdcfcbef335290b2b49e6bd86e2eb0
borajski <56654538+borajski@users.noreply.github.com>
2024-06-10 11:39:12 +0200

    Izmjena Home kontrolera za ispis zadnjih 7 dana bez današnjeg dana

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,16 +25,19 @@ class HomeController extends Controller
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
-    {
-        $user_id = auth()->user()->id;
-        $query = (new Menu())->newQuery();
+{
+    $user_id = auth()->user()->id;
+    $query = (new Menu())->newQuery();
 
-        $dnevni_unos = $query->where('user_id',$user_id)
-                             ->whereDate('created_at', '>=', now()->subDays(7))
-                             ->get();
-        $foodItems = Food::all();
-        $mealItems = Meal::all();
-        return view('home')->with(['foodItems' => $foodItems, 'mealItems' => $mealItems, 'menus' => $dnevni_unos]);
- 
-    }
+    $dnevni_unos = $query->where('user_id', $user_id)
+                         ->whereDate('created_at', '>=', now()->subDays(7))
+                         ->whereDate('created_at', '<', now())
+                         ->get();
+    
+    $foodItems = Food::all();
+    $mealItems = Meal::all();
+    
+    return view('home')->with(['foodItems' => $foodItems, 'mealItems' => $mealItems, 'menus' => $dnevni_unos]);
+}
+
 }
